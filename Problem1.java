a2804e93751d33286a632af69e955617609fe520
ashish2097 <ashishduklann@gmail.com>
2019-06-04 09:10:34 +0530

    Second Solution

---
+++ Problem1.java
@@ -0,0 +1,31 @@
+﻿import java.util.HashSet;
+
+import java.util.Set;
+
+
+public class Main
+{
+
+	public static void main(String[] args) {
+
+	    Integer[] arr = new Integer[]{10, 15, 3, 7};
+
+	    int k = 17;
+
+	    HashSet<Integer> map = new HashSet<>();
+	    int i;
+	    for(i = 0; i < arr.length; i++) {
+	        Integer remaining = k - arr[i];
+	        if(map.contains(arr[i])) {
+	            break;
+	        }
+	        else {
+	            map.add(remaining);
+	        }
+	    }
+	    if(i == arr.length)
+	        System.out.println("false");
+	    else
+	        System.out.println("true");
+	}
+}
---
+++ Problem2.java
@@ -0,0 +1,34 @@
+﻿public class HelloWorld{
+
+    public static void main(String []args){
+        Integer[] arr = new Integer[]{1, 2, 3, 4, 5};
+        int len = arr.length;
+        
+        Integer[] a = new Integer[len];
+        Integer[] b = new Integer[len];
+        Integer[] ans = new Integer[len];
+        
+        
+        
+        for(int i = 0; i < len; i++) {
+            a[i] = 1;
+            b[i] = 1;
+        }
+        for(int i = 0; i < len; i++) {
+            int j = len - i - 1;
+            if(i == 0) {
+                a[i] = 1;
+                b[j] = 1;
+            }
+            else {
+                a[i] = a[i-1] * arr[i-1];
+                b[j] = b[j+1] * arr[j+1];
+            }
+        }
+        for(int i = 0; i < len; i++) {
+            ans[i] = a[i] * b[i];
+            System.out.println(ans[i]);
+        }
+        
+    }
+}
