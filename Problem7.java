2c7209ab724fd18eb651f865d1ed1a1bad87bf8c
ashish2097 <ashishduklann@gmail.com>
2019-06-13 20:18:19 +0530

    Solution 7

---
+++ Problem7.java
@@ -0,0 +1,19 @@
+﻿
+public class Main
+{
+    public static int func(String s, int i) {
+        if(i == s.length())
+            return 1;
+        
+        if(i < s.length()-1 && Integer.parseInt(s.substring(i, i+2)) <= 26) {
+                return func(s, i+1) + func(s, i+2);
+        }
+        
+        return func(s, i+1);
+    }
+	public static void main(String[] args) {
+        String s = new String("111");
+        int ans = func(s, 0);
+        System.out.println(ans);
+	}
+}
