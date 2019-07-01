d5b82eba7d78247dc5f6ae2014aeb7eb4a96c98e
ashish2097 <ashishduklann@gmail.com>
2019-07-01 08:58:34 +0530

    Solution 5

---
+++ Problem5.java
@@ -0,0 +1,20 @@
+﻿def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+def car(g):
+    def left(a,b):
+        return a
+    return g(left)
+
+print(car(cons(1,2)))
+
+def cdr(g):
+    def right(a,b):
+        return b
+    return g(right)
+
+print(cdr(cons(1,2)))
+
+#car(cons(1,2))=> car(pair)=> pair(left) => left(1,2)=> 1
